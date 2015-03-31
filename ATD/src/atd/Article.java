04414f721daf5ec7cc69274f9f683e7035df8b2a
Deadwalker09 <nigelvh@hotmail.com>
2015-03-31 21:11:19 +0200

    Article

--- ATD/src/atd/Article.java
+++ ATD/src/atd/Article.java
@@ -1,5 +1,55 @@
 package atd;
 
 public class Article {
+    private String name;
+    private int number;
+    private int inStock;
+    private double price;
     
+    public Article(String name, int number, double Price) {
+        this.name = name;
+        this.number = number;
+        this.price = Price;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public int getInStock() {
+        return inStock;
+    }
+    
+    public void setPrice(double newPrice) {
+        price = newPrice;
+    }
+    
+    public void setInStock(int countedStock) {
+        inStock = countedStock;
+    }
+    
+    public void stockIn(int newStock) {
+        inStock += newStock;
+    }
+    
+    public void StockOut(int lostStock) {
+        inStock -= lostStock;
+    }
+    
+    /* TODO: 
+    public InvoiceLine getInvoiceLine() {
+        new InvoiceLine(this.toString(), price);
+    }*/
+    
+    public String toString() {
+        return name + " costs " + price;
+    }
 }
